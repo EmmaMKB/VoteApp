cbc33a47be77768082a2faccc9389723f920b774
= <=>
2022-05-02 00:10:23 +0200

    ✨ Design signin page

---
+++ signin.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vote Now</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="main">
+        <header class="navbar">
+            <a href="#" class="logo"><h3>Vote App</h3></a>
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Votes</a></li>
+                <li><a href="">Connexion</a></li>
+                <li><a href="">Inscription</a></li>
+            </ul>
+        </header>
+        <section class="login-form">
+            <div class="content">
+                <h1>Inscription</h1>
+                <form class="form" action="" method="post">
+                    <div class="input">
+                        <label for="">Nom</label>
+                        <input type="email" name="" placeholder="Votre Nom Complet" id="">
+                    </div>
+                    <div class="input">
+                        <label for="">Mail</label>
+                        <input type="email" name="" placeholder="Votre Adresse Mail" id="">
+                    </div>
+                    <div class="input">
+                        <label for="">Mail</label>
+                        <input type="tel" name="" placeholder="Votre Numero de Tel" id="">
+                    </div>
+                    <div class="input">
+                        <label for="">Mot de Passe</label>
+                        <input type="password" name="" placeholder="Votre Mot de Passe" id="">
+                    </div>
+                    <div class="input">
+                        <label for="">Confirmer le Mot de Passe</label>
+                        <input type="password" name="" placeholder="Votre Mot de Passe" id="">
+                    </div>
+                    <button class="orange-btn">S'inscrire</button>
+                </form>
+            </div>
+        </section>
+        <footer>
+            <div class="content">
+                <div class="app-description">
+                    <h3>Vote App</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque 
+                        quis aliquid illum fuga id enim, hic commodi repellat maiores. Mollitia, 
+                        laborum sequi delectus eaque nam ipsam! Adipisci, enim. Optio, sequi.</p>
+                </div>
+                <div class="social">
+                    <ul>
+                        <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                        <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                        <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+                <div class="copyright">
+                    <p>Tous droits réservés Jean-Baptiste Lubilu</p>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
