9904890718f55c69648aedd1427d1a91db632950
= <=>
2022-05-01 23:52:33 +0200

    ✨ Home page Design

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vote Now</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
 </head>
 <body>
     <div class="main">
@@ -13,11 +14,60 @@
             <a href="#" class="logo"><h3>Vote App</h3></a>
             <ul>
                 <li><a href="">Accueil</a></li>
-                <li><a href="">About</a></li>
+                <li><a href="">Votes</a></li>
                 <li><a href="">Connexion</a></li>
                 <li><a href="">Inscription</a></li>
             </ul>
         </header>
+        <div class="banner">
+            <div class="content">
+                <div class="text">
+                    <h1 class="main-title">
+                        Meilleure plateforme de vote
+                    </h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid quia atque, rem ratione nemo ad. Eligendi quae adipisci quos id quaerat quisquam blanditiis, distinctio ab dolorem asperiores sed velit veniam!</p>
+                    <a class="orange-btn" href="">En Savoir Plus</a>
+                </div>
+                <div class="votes">
+                    <h3>Derniers votes organisés</h3>
+                    <div class="votes-section">
+                        <div class="vote-item">
+
+                        </div>
+                    </div>
+                    <a class="orange-btn" href="">Tous les votes</a>
+                </div>
+            </div>
+        </div>
+        <section class="about">
+            <div class="content">
+                <h1>A Propos de la plateforme</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est mollitia, 
+                    voluptates fuga vitae magni veniam iure, voluptatem ipsa qui ratione 
+                    perspiciatis esse. Enim eum officiis quisquam sequi ipsam quae voluptas!</p>
+                <a href="" class="orange-btn">Se connecter</a>
+            </div>
+        </section>
+        <footer>
+            <div class="content">
+                <div class="app-description">
+                    <h3>Vote App</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque 
+                        quis aliquid illum fuga id enim, hic commodi repellat maiores. Mollitia, 
+                        laborum sequi delectus eaque nam ipsam! Adipisci, enim. Optio, sequi.</p>
+                </div>
+                <div class="social">
+                    <ul>
+                        <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                        <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                        <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+                <div class="copyright">
+                    <p>Tous droits réservés Jean-Baptiste Lubilu</p>
+                </div>
+            </div>
+        </footer>
     </div>
 </body>
 </html>
