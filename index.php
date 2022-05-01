2e07fefa664c68b522bbf2f6bf000c71a9b81b64
= <=>
2022-05-01 23:07:15 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vote Now</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="main">
+        <header class="navbar">
+            <a href="#" class="logo"><h3>Vote App</h3></a>
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Connexion</a></li>
+                <li><a href="">Inscription</a></li>
+            </ul>
+        </header>
+    </div>
+</body>
+</html>
