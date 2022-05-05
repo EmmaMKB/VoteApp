3610ca0ba3218acd3e83d7353ad51652fba9e981
emmamkb <emmanuelmakabu7@gmail.com>
2022-05-05 18:16:10 +0200

    ✨ Implement User registration

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -2,11 +2,28 @@
 
 namespace Controllers;
 use Models\UserModel;
+require_once(__DIR__.'\..\models\UserModel.php');
+
+if ($_POST) {
+    if ($_POST['action']=="post_user") {
+        UserController::register();
+    }
+}
 
 class UserController {
 
     public static function register()
     {
-        # code...
+        if (strcmp($_POST['password'], $_POST['pwdconf']) == 0) {
+            $data = new UserModel();
+
+            $data->__init($_POST['name'], $_POST['email'], $_POST['password'], $_POST['tel']);
+
+            $data->new();
+
+            return header("Location: /login.php");
+        }
+
+        header("Location: /signin.php");
     }
 }
